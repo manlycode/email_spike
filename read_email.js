4e9288dbaf7f1357d324fddb75eb8d07367a236d
Chris Rittersdorf <manlycode@gmail.com>
2010-12-18 15:03:10 -0500

    Added better eventedness.
    Also added random waits for processing to
    prove evented behavior.

--- read_email.js
+++ read_email.js
@@ -7,12 +7,18 @@ var ImapConnection = require('imap').ImapConnection,
       port: 993,
       secure: true
     });
-    
+
+
 var reportError = function(err) {
   console.log('There was an error');
   console.dir(err);
   process.exit(1);
 };
+
+var options = { "request": {
+                  "headers": false, "body": true}};
+
+var emails_to_process = 0;
 
 imap.connect(function(err) {
   if (err) { reportError(err); }
@@ -21,23 +27,65 @@ imap.connect(function(err) {
   imap.openBox('INBOX', false, function(box) {
     console.log("Searching the inbox");
     imap.search(['UNDELETED'], function(err, email_ids) {
-      var options = { "request": {
-                        "headers": false, "body": true}};
-      console.log("fetching emails");
-      imap.fetch(email_ids, options, function(err, emails) {
-        process.emit('emails-found', emails);
-      });
+      
+      emails_to_process = email_ids.length;
+
+      for (var i=0; i <  email_ids.length; i++) {
+        process.emit('fetch-email', email_ids[i]);
+      }
+
+      console.log('returning from search');
     });
   });  
 });
 
-process.on('emails-found', function(emails) {
-  console.log("found some emails");
-  console.dir(emails);
-  process.emit('emails-finished');
+process.on('fetch-email', function(id) {
+  imap.fetch(id, options, function(err, emails){
+    process.emit('handle-email', emails[0]);
+  });
 });
 
-process.on('emails-finished', function() {
-  console.log("finished them");
-  imap.logout();
+process.on('handle-email', function(email){
+  var f = function() {
+    console.log('Handled email: ' +  email.id);
+    process.emit('finished-handling');
+  };
+  
+  if (email.id == 5 || email.id == 7) {
+    setTimeout(f, 10000*Math.random());
+  } else {
+    f();
+  }
+});
+
+process.on('finished-handling', function() {
+  emails_to_process--;
+  if(emails_to_process <= 0) {
+    imap.logout();
+  }
+});
+
+process.on('start-processing', function() {
+  for (var i=0; i <  emails_to_process.length; i++) {
+    process.emit('process-email', emails_to_process[i]);
+  }
+});
+
+process.on('process-email', function(id) {
+  console.log("Fetching: " + id);
+
+  imap.fetch(id, options, function(err, email) {
+    process.emit('finished-processing-email', email);
+  });
+
+  for (var i=0; i <  emails_to_process.length; i++) {
+    console.log("trying to remove: " + id);
+    console.dir(emails_to_process);
+    if (id.toString() == emails_to_process[i]) {
+      emails_to_process.splice(i, 1);
+      break;
+    }
+  }
+  
+  process.emit('finished-processing-email');
 });
